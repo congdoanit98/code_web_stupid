412096c2ff3f96f8acf5c1d29a106135228ecf15
Cong Doan <congdoanit98@gmail.com>
2021-03-21 11:44:58 +0700

    add project into web

--- main.js
+++ main.js
@@ -58,3 +58,49 @@ ScrollTrigger.create({
     //   console.log("progress:", self.progress.toFixed(3), "direction:", self.direction, "velocity", self.getVelocity());
     // }
 });
+
+const projectIntro = gsap.from(".project-intro", {
+    delay: 0.25,
+    x: 200,
+    duration: 2,
+    opacity: 0,
+    ease: "power3",
+    stagger: 0.5
+});
+ScrollTrigger.create({
+    trigger: ".project",
+    animation: projectIntro,
+    // Uncomment these to see how they affect the ScrollTrigger
+    // markers: true,
+    start: "top 450px",
+    // end: "top 100px",
+    // toggleClass: "active",
+    // pin: true,
+    // scrub: 1,
+    // onUpdate: self => {
+    //   console.log("progress:", self.progress.toFixed(3), "direction:", self.direction, "velocity", self.getVelocity());
+    // }
+});
+
+const project = gsap.from(".project-item", {
+    delay: 0.25,
+    y: 100,
+    duration: 2,
+    opacity: 0,
+    ease: "power3",
+    stagger: 0.5
+});
+ScrollTrigger.create({
+    trigger: ".project",
+    animation: project,
+    // Uncomment these to see how they affect the ScrollTrigger
+    // markers: true,
+    start: "top 500px",
+    // end: "top 100px",
+    // toggleClass: "active",
+    // pin: true,
+    // scrub: 1,
+    // onUpdate: self => {
+    //   console.log("progress:", self.progress.toFixed(3), "direction:", self.direction, "velocity", self.getVelocity());
+    // }
+});
